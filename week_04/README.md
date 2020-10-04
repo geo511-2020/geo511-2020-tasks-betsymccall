ef512d338978e4f06a7db37a0b91e38df415b3ab
Betsy McCall <betsymcc@buffalo.edu>
2020-10-03 20:08:50 -0400

    case study 5 upload

--- week_04/README.md
+++ week_04/README.md
@@ -1,17 +1,2 @@
-Useful functions:
+ filter() is nice because you can select (or by extension, remove) entries. mutate was useful in this week's case study because the join needed the column names to match (I couldn't make it work otherwise). select() is nice because you can get rid of columns you don't need.
 
-As a "math" person, I am particularly fond of eigen(),
-because finding eigenvalues and eigenvectors by hand is 
-horribly tedious.
-
-The function x11() opens up a new window for a plot,
-which can be nice for viewing images that aren't quite
-so crammed into the little viewing window on R studio.
-
-sample() is useful for selecting data, especially if you are doing training/testing or want a simulation.
-
-Is this what you meant, or did you mean something related to data wrangling?
-
-If that's the case, then I'd have to say something like filter() is nice because you can select (or by extension, remove) entries. mutate was useful in this week's case study because the join needed the column names to match (I couldn't make it work otherwise). select() is nice because you can get rid of columns you don't need.
-
-The directions could be more specific.
---
+++ week_05/script5.R
@@ -0,0 +1,39 @@
+###################################################
+##Case Study #5, GEO 511
+##Author: Betsy McCall
+##Date: 10/03/2020
+##Revised: 
+###################################################
+library(spData)
+library(sf)
+library(tidyverse)
+library(units)
+
+data(world)
+data(us_states)
+
+#transform coordinates
+albers="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+worlda <- st_transform(world, crs=albers)
+#filter to Canada only, and add buffer of 10km
+worldac <- worlda %>% filter(name_long=="Canada")
+worldacb <- st_buffer(worldac, dist = 10000)
+#transform US map
+us_statesa <- st_transform(us_states, crs=albers)
+#filter to NY state only
+us_statesany <- us_statesa %>% filter(NAME=="New York")
+
+border_10km <- st_intersection(worldacb,us_statesac)
+#plot 10km border of Canada in NY
+ggplot(data=us_statesany)+geom_sf()+geom_sf(data=border_10km,fill="red")
+
+#find area
+border_area <- st_area(border_10km)
+print(border_area)
+#convert to kilometers-squared
+#border_area_km2 <- border_area/10^6
+#print(border_area_km2)
+units(border_area) <- as_units("km^2")
+print(border_area)
+
